d7d3c62f5cf3b99af76932e9c4124c047107a351
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-10-17 14:07:38 -0400

    Hobbit name test passes and fails when it should.

--- hobbits/hobbitsModel.spec.js
+++ hobbits/hobbitsModel.spec.js
@@ -3,7 +3,7 @@ const Hobbits = require('./hobbitsModel');
 
 describe('hobbits model', () => {
     beforeEach(async () => {
-        await db('hobbits').truncate();
+        await db('hobbits').truncate(); // cleans up table before running insert method
     });
 
     it('Is using testing environment', () => {
@@ -17,6 +17,11 @@ describe('hobbits model', () => {
             await Hobbits.insert({ name: 'Sam' })
             const hobbits = await db('hobbits');
             expect(hobbits).toHaveLength(1);
+        });
+
+        it('Should add provided hobbit to database', async () => {            
+            let hobbit = await Hobbits.insert({ name: 'Sam' });
+            expect(hobbit.name).toBe('Sam');
         });
     });
 });
