e4bcac70dc3ca769129a997ab1f8390234a31066
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-10-17 12:49:35 -0400

    GET test behaving as expected.

---
+++ api/server.spec.js
@@ -0,0 +1,14 @@
+const request = require('supertest');
+const server = require ('./server');
+
+describe('GET /', () => {
+    // Should return http 200
+    it('should return 200', () => {
+        return request(server).get('/').then(res => { // must return request to prevent false positive
+            expect(res.status).toBe(200)
+        })
+    })
+    // Should return response in JSON
+    // Should return object with API value 'Up'
+
+})
