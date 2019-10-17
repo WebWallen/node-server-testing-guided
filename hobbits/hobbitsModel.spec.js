60e913025ce9fee4d008696265e274797d78b824
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-10-17 14:01:24 -0400

    BeforeEach truncate function works.

--- hobbits/hobbitsModel.spec.js
+++ hobbits/hobbitsModel.spec.js
@@ -2,12 +2,16 @@ const db = require('../data/dbConfig');
 const Hobbits = require('./hobbitsModel');
 
 describe('hobbits model', () => {
+    beforeEach(async () => {
+        await db('hobbits').truncate();
+    });
+
     it('Is using testing environment', () => {
         expect(process.env.DB_ENV).toBe('testing'); // see package.json for environment related code under "scripts.test"
     });
 
     describe('Insert method', () => {
-        it('Should add hobbit to database', async () => {
+        it('Should add hobbit to database', async () => {            
             const records = await db('hobbits');
             expect(records).toHaveLength(0);
             await Hobbits.insert({ name: 'Sam' })
