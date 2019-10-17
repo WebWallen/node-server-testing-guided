479acc57b744724e7fde4a1bb2d304658883fde5
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-10-17 13:49:29 -0400

    Test for inserting hobbit passed (then failed after inserting a hobbit because there's no longer 0). Sounds right.

--- hobbits/hobbitsModel.spec.js
+++ hobbits/hobbitsModel.spec.js
@@ -1,5 +1,18 @@
+const db = require('../data/dbConfig');
+const Hobbits = require('./hobbitsModel');
+
 describe('hobbits model', () => {
     it('Is using testing environment', () => {
-        expect(process.env.DB_ENV).toBe('testing')
-    })
-})
+        expect(process.env.DB_ENV).toBe('testing'); // see package.json for environment related code under "scripts.test"
+    });
+
+    describe('Insert method', () => {
+        it('Should add hobbit to database', async () => {
+            const records = await db('hobbits');
+            expect(records).toHaveLength(0);
+            await Hobbits.insert({ name: 'Sam' })
+            const hobbits = await db('hobbits');
+            expect(hobbits).toHaveLength(1);
+        });
+    });
+});
